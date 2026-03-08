4a15e8986c257e2566edbcc2ea9bd1be13150de2
thomasleonelliwendling-ship-it <thomasleonelliwendling@gmail.com>
2026-03-08 18:10:31 +0100

    Delete Project Quantum Algo directory

--- Project Quantum Algo/Algorithm/Classical_Algorithm/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Exhaustive_search import exhaustive_search, exhaustive_search_parallel
-from .Random_search import random_search
-from .Local_search import local_search
-from .Random_local_search import random_local_search
--- Project Quantum Algo/Problem/__init__.py
+++
@@ -1,6 +0,0 @@
-from .MaxCutProblem import MaxCutProblem
-from .QuboProblem import QuboProblem
-from .IsingProblem import IsingProblem
-from .KnapsackProblem import KnapsackProblem
-from .TspProblem import TspProblem
-from .Converter import qubo_to_ising, ising_to_qubo
